22d995a4cb98484f7b3651a7efe699ba41894a9f
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2022-09-11 23:22:21 -0600

    Routing configurado

--- Frontend/tallerTEC/src/app/app-routing.module.ts
+++ Frontend/tallerTEC/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'', pathMatch:'full'},
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/tallerTEC/src/app/app.module.ts
+++ Frontend/tallerTEC/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';  
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './plantillas/header/header.component';
+import { FooterComponent } from './plantillas/footer/footer.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { MenuComponent } from './vistas/menu/menu.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    MenuComponent,
+    NuevoComponent,
+    EditarComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
