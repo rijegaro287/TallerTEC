fed7534f08ac899db45d5456c084be8f6eb53527
cuadriante <adriana@sinapsis.ws>
2022-09-15 16:36:30 -0600

    tablas

--- Frontend/tallerFront/src/app/vistas/dashboard/dashboard.component.ts
+++ Frontend/tallerFront/src/app/vistas/dashboard/dashboard.component.ts
@@ -1,20 +1,35 @@
-import { ConstantPool } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 import { ApiService } from 'src/app/servicios/api/api.service';
+
+import { ListaEmpleadosI } from '../../modelos/listaempleados.interface'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+
 export class DashboardComponent implements OnInit {
 
-  constructor(private api:ApiService) { }
+  empleados: ListaEmpleadosI[] = [];
+  
+  constructor(private api:ApiService, private router:Router) { 
+  }
 
   ngOnInit(): void {
     this.api.getAllEmpleados().subscribe(data =>{
-      console.log(data);
+      console.log(data)
     })
   }
 
+  editarEmpleado(id: any){
+    this.router.navigate(['editar', id])
+  }
+
+  nuevoEmpleado(){
+    this.router.navigate(['nuevo']);
+
+  }
 }
--- Frontend/tallerFront/src/app/vistas/login/login.component.ts
+++ Frontend/tallerFront/src/app/vistas/login/login.component.ts
@@ -24,6 +24,13 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  checkLocalStorage(){
+    if (localStorage.getItem('token')){
+      this.router.navigate(['mainmenu'])
+    }
+  
+  }
+  
   onLogin(form:any){
     this.api.loginID(form).subscribe(data =>
       {
