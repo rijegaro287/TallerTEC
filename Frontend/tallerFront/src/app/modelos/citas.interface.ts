f8a85e28ed2a88ab8c446faa18eb1773cde0ebbc
cuadriante <adriana@sinapsis.ws>
2022-09-16 16:38:18 -0600

    uruutrcf

---
+++ Frontend/tallerFront/src/app/modelos/citas.interface.ts
@@ -0,0 +1,14 @@
+export interface CitaI{
+    ID:number;
+    Date:string;
+    Time:string;
+    AttendedClientID:number;
+    LicensePlate:string;
+    BranchID:number;
+    RequiredService:number;
+    MechanicID:number;
+    AssistantID:number;
+    NecessaryParts:Array<number>;
+
+}
+
