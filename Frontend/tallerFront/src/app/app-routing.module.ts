fbcc7c35daf6cded3389d2033d1501f1ff5dc0bc
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2022-09-15 18:03:33 -0600

    dashboard cliente terminado

--- Frontend/tallerFront/src/app/app-routing.module.ts
+++ Frontend/tallerFront/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './vistas/login/login.component';
 import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { DashboardComponent2 } from './vistas/dashboard2/dashboard2.component';
 import { NuevoComponent } from './vistas/nuevo/nuevo.component';
 import { EditarComponent } from './vistas/editar/editar.component';
 import { MainmenuComponent } from './vistas/mainmenu/mainmenu.component';
@@ -10,8 +11,9 @@ const routes: Routes = [
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'login', component:LoginComponent},
   {path:'dashboard', component:DashboardComponent},
+  {path:'dashboard2', component:DashboardComponent2},
   {path:'nuevo', component:NuevoComponent},
-  {path:'editar/:id', component:EditarComponent},
+  {path:'editar', component:EditarComponent},
   {path:'mainmenu', component:MainmenuComponent}
 
 ];
@@ -21,4 +23,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, DashboardComponent, NuevoComponent, EditarComponent, MainmenuComponent]
+export const routingComponents = [LoginComponent, DashboardComponent, DashboardComponent2,NuevoComponent, EditarComponent, MainmenuComponent]
---
+++ Frontend/tallerFront/src/app/modelos/listaClientes.interface.ts
@@ -0,0 +1,8 @@
+export interface ListaClientesI{
+    ID:number,
+    Name:string,
+    LastName:string,
+    Email:string,
+    PhoneNumber:number,
+    Address:string
+}
